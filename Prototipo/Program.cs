966509ed76895ee0974a6e5bbc5d8620655caef5
hugoaraujor <hugoaraujo@hotmail.com>
2017-08-27 20:44:39 -0400

    Prototype Pattern

---
+++ Prototipo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prototipo;
+namespace Prototipo
+{
+
+	class Program
+	{
+		public static void ManejaVehiculos()
+		{
+			vehiculo vehiculoa = new vehiculo();
+			vehiculoa.getvehiculo("mitsubishi", 1990);
+			Console.WriteLine("vehiculob es igual al a");
+			vehiculoa.display();
+		
+			vehiculo vehiculob = (vehiculo)vehiculoa.Clone();
+			Console.WriteLine("Despues de la clonacion");
+			Console.WriteLine("vehiculo b");
+			vehiculoa.display();
+			vehiculob.display();
+
+			vehiculob.Marca = "Ford";
+			Console.WriteLine("Cambio marca del vehiculo b");
+			vehiculoa.display();
+			vehiculob.display();
+
+			
+			
+		}
+
+		static void Main(string[] args)
+		{
+		     ManejaVehiculos();
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Prototipo/vehiculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototipo
+{
+	public class vehiculo:ICloneable
+	{
+		public string Marca { get; set; }
+		public int Año { get; set; }
+
+		public object Clone()
+		{
+			return this.MemberwiseClone();
+		}
+		public void display()
+		{
+			Console.WriteLine("Marca vehiculo " + this.Marca + " año:" + this.Año.ToString());
+		
+		}
+		public void getvehiculo(string marca,int año)
+		{
+			this.Marca = marca;
+			this.Año = año;
+		}
+	}
+	
+
+}
