03f130b342e0c1ee5dba30370a7905b93158cbee
hugoaraujor <hugoaraujo@hotmail.com>
2017-08-28 20:35:27 -0400

    Singleton Sample Initial COmmit

---
+++ SingletonSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			SingletonSample singleton = SingletonSample.Instance;
+			singleton.DisplayMessage();
+			Console.ReadLine();
+
+		}
+	}
+}
---
+++ SingletonSample/SingletonSample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSample
+{
+	public class SingletonSample
+	{
+        private static SingletonSample instance;
+		private static readonly object swlock = new object();
+
+		private SingletonSample()
+		{
+
+		}
+		
+
+		public void DisplayMessage()
+		{
+			Console.WriteLine("This is the SingleTon Sample Program");
+		}
+	
+		public static SingletonSample Instance
+		{
+
+ 		get
+          {
+				lock (swlock)
+				{
+					if (instance == null)
+					{
+						instance = new SingletonSample();
+					}
+					return instance;
+				}
+			}
+
+		}
+	}
+	
+	
+}
